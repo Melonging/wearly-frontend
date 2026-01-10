61e497cc08134db019774f1a392944e78bb8073a
chanbii <2039215k@naver.com>
2026-01-10 10:52:02 +0900

    [feat]: 옷 사진 업로드 UI 구현(옷 사진 등록 & 자동 분류)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,15 +33,22 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    viewBinding{
+        enable=true
+    }
 }
 
 dependencies {
+    // Glide
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.recyclerview)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
