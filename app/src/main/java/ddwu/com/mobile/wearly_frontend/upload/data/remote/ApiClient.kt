32fab16cb55d2414609d48e732958da51f06908c
chanbii <2039215k@naver.com>
2026-02-10 06:53:46 +0900

    [feat]이미지 업로드 API 연결 및 로딩 화면 초기 구현

---
+++ app/src/main/java/ddwu/com/mobile/wearly_frontend/upload/data/remote/ApiClient.kt
@@ -0,0 +1,46 @@
+package ddwu.com.mobile.wearly_frontend.upload.data.remote
+
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object ApiClient {
+    var BASE_URL: String = "***REMOVED***"
+
+
+    fun createUploadApi(tokenProvider: () -> String): UploadApi {
+
+        val logger = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        val authInterceptor = Interceptor { chain ->
+            val token = tokenProvider().trim()
+            val req = chain.request().newBuilder()
+                .addHeader("Authorization", "Bearer $token")
+                .build()
+            chain.proceed(req)
+        }
+
+        val client = OkHttpClient.Builder()
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .addInterceptor(authInterceptor)
+            .addInterceptor(logger)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://192.168.45.195:4000/")
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        return retrofit.create(UploadApi::class.java)
+    }
+
+}
+
